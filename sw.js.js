da02e65cb9bc6d764b2517c9aa0cbe149c5bbf8b
MADHUSMITA315 <madhusmitamohanta315@gmail.com>
2026-07-06 20:09:58 +0530

    Add files via upload
    INITIAL UPLOAD

---
+++ sw.js.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'aura-v1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['/', '/index.html', '/manifest.json', '/icon-512.png'])));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
